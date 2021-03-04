6d574bc7e9a6eabf3695b507ddc64b43ef332823
emily farrow <ejfarrow@dons.usfca.edu>
2021-03-04 11:31:24 -0800

    Updated Code and Assets

--- sketch.js
+++ sketch.js
@@ -1,23 +1,293 @@
 /*************************************************************************
-    (*)Project Name
-          (*)by author's name/s
+    (*)Project 1: Plant Lovers Unanimous
+          (*)by Emily Farrow
 
-    (*)Overview - what the project does...
+    (*)Overview - Website created to give tips on plant care based on expertise and skill level. 
+    There are 5 pages, the first being the main page, second through fourth being the different skill level pages,
+    and the last being the 'Things to buy' page.
  
-    ---------------------------------------------------------------------
-    (*)Notes: (ie: To be fixed, I Need Help With, Clarify This, etc etc: )
-     (1)
-     (2)
 **************************************************************************/
 
 
-// Setup code goes here
+// variable that is a function 
+var drawFunction;
+
+// rounded corner variable
+var crnr = 4;
+
+//array variables
+var mainPageAssets = [];
+var beginnerAssets = [];
+var intermediateAssets = [];
+var advancedAssets = [];
+var thingstobuyAssets = [];
+
+//beginner through advanced variables
+var maintitleX = 112;
+var maintitleY = 80; 
+
+var plant1X = 110;
+var plant1Y = 220;
+
+var plant2X = 110; 
+var plant2Y = 740;
+
+var text1X = 574;
+var text1Y = 220;
+
+var text2X = 574;
+var text2Y = 740;
+
+var button1X = 574;
+var button1Y = 587;
+
+var button2X = 574;
+var button2Y = 1121;
+
+// next page variables
+var nextY = 1270; 
+
+
+//things to buy variables
+var imageleftX = 228;
+var row1Y = 240;
+var imagerightX = 686;
+var row2Y = 810;
+
+var textrow1Y = 583;
+var textrow2Y = 1153;
+
+//navigation
+var navKey = [];
+
+//preload images in array
+function preload() {
+
+  //main page images
+  mainPageAssets[0] = loadImage('assets/mainpage1.png');
+  mainPageAssets[1] = loadImage('assets/mainpage2.png');
+
+ //beginner images
+  beginnerAssets[0] = loadImage('assets/beginnertitle.png');
+  beginnerAssets[1] = loadImage('assets/cactus.png');
+  beginnerAssets[2] = loadImage('assets/cactusbodytext.png');
+  beginnerAssets[3] = loadImage('assets/buysoil.png');
+  beginnerAssets[4] = loadImage('assets/pothos.png');
+  beginnerAssets[5] = loadImage('assets/pothosbodytext.png');
+  beginnerAssets[6] = loadImage('assets/buymacrame.png');
+  beginnerAssets[7] = loadImage('assets/gotointermediate.png');
+
+  
+  //intermediate images
+  intermediateAssets[0] = loadImage('assets/intermediatetitle.png');
+  intermediateAssets[1] = loadImage('assets/monstera.png');
+  intermediateAssets[2] = loadImage('assets/monsterabodytext.png');
+  intermediateAssets[3] = loadImage('assets/buysoil.png');
+  intermediateAssets[4] = loadImage('assets/africanmask.png');
+  intermediateAssets[5] = loadImage('assets/africanmaskbodytext.png');
+  intermediateAssets[6] = loadImage('assets/buysoil.png');
+  intermediateAssets[7] = loadImage('assets/gotoadvanced.png');
+  
+  //advanced images
+  advancedAssets[0] = loadImage('assets/advancedtitle.png');
+  advancedAssets[1] = loadImage('assets/calathea.png');
+  advancedAssets[2] = loadImage('assets/calatheabodytext.png');
+  advancedAssets[3] = loadImage('assets/buymacrame.png');
+  advancedAssets[4] = loadImage('assets/fiddleleaf.png');
+  advancedAssets[5] = loadImage('assets/fiddleleafbodytext.png');
+  advancedAssets[6] = loadImage('assets/buymacrame.png');
+  advancedAssets[7] = loadImage('assets/gotomainpage.png');
+
+  //things to buy images
+  thingstobuyAssets[0] = loadImage('assets/thingstobuytitle.png');
+  thingstobuyAssets[1] = loadImage('assets/macrame.png');
+  thingstobuyAssets[2] = loadImage('assets/humidifier.png');
+  thingstobuyAssets[3] = loadImage('assets/perlite.png');
+  thingstobuyAssets[4] = loadImage('assets/spritzer.png');
+  thingstobuyAssets[5] = loadImage('assets/macrametext.png');
+  thingstobuyAssets[6] = loadImage('assets/humidifiertext.png');
+  thingstobuyAssets[7] = loadImage('assets/perlitetext.png');
+  thingstobuyAssets[8] = loadImage('assets/spritzertext.png');
+
+}
+
+// Center drawing, drawFunction will be one for default
 function setup() {
-  createCanvas(windowWidth, windowHeight);
- }
+  createCanvas(1280, 1422);
+  textAlign(CENTER);
+  textSize(28);
+  noStroke();
 
+  //setting the navigation key array
+  navKey[0] = ('[m]');
+  navKey[1] = ('[b]');
+  navKey[2] = ('[i]');
+  navKey[3] = ('[a]');
+  navKey[4] = ('[t]');
 
-// Draw code goes here
+  // startup room
+  drawFunction = drawMainpage;
+
+}
+
+//calls your state machine function
 function draw() {
-  background(0);
+  drawFunction();
+}
+
+//draws images from mainpageAssets array
+drawMainpage = function() {
+   background(239, 237, 180);
+   imageMode(CENTER);
+
+   //images in array
+   image(mainPageAssets[0], windowWidth/2, windowHeight/2.5);  //plant lovers unanimous title
+   image(mainPageAssets[1], windowWidth/2, windowHeight/1.75);  //find your plant care level image
+   
+}
+
+//draws images from beginnerAssets array
+drawBeginner = function() {
+
+  background(255, 232, 182);  
+  imageMode(CORNER);
+   //images in array
+   image(beginnerAssets[0], maintitleX, maintitleY);  //subtitle
+   image(beginnerAssets[1], plant1X, plant1Y); //first plant
+   image(beginnerAssets[2], text1X, text1Y);  //paragraph 1
+   image(beginnerAssets[3], button1X, button1Y); //suggestion button
+   image(beginnerAssets[4], plant2X, plant2Y); //plant 2
+   image(beginnerAssets[5], text2X, text2Y);  //paragraph 2
+   image(beginnerAssets[6], button2X, button2Y); //suggestion button
+   image(beginnerAssets[7], button2X, nextY); //next page button
+
+   //Create clickable object
+   //cactibutton = new Clickable();
+
+   //set image to this png
+   //cactibutton.image = beginnerAssets[3];
+
+   //button background
+   //cactibutton.color = "#FFFFFF";
+
+   // set button width and height to image size
+   //cactibutton.width = beginnerAssets[3].width;
+   //cactibutton.height = beginnerAssets[3].height;
+}
+
+//draws images from intermediateAssets array
+drawIntermediate = function() {
+ background(255, 232, 182); 
+
+   //images in array
+   image(intermediateAssets[0], maintitleX, maintitleY);  //subtitle
+   image(intermediateAssets[1], plant1X, plant1Y); //first plant
+   image(intermediateAssets[2], text1X, text1Y);  //paragraph 1
+   image(intermediateAssets[3], button1X, button1Y); //suggestion button
+   image(intermediateAssets[4], plant2X, plant2Y); //plant 2
+   image(intermediateAssets[5], text2X, text2Y);  //paragraph 2
+   image(intermediateAssets[6], button2X, button2Y); //suggestion button
+   image(intermediateAssets[7], button2X, nextY); //next page button
+}
+
+//draws images from advancedAssets array
+drawAdvanced = function() {
+ background(255, 232, 182);  
+
+   //images in array
+   image(advancedAssets[0], maintitleX, maintitleY);  //subtitle
+   image(advancedAssets[1], plant1X, plant1Y); //first plant
+   image(advancedAssets[2], text1X, text1Y);  //paragraph 1
+   image(advancedAssets[3], button1X, button1Y); //suggestion button
+   image(advancedAssets[4], plant2X, plant2Y); //plant 2
+   image(advancedAssets[5], text2X, text2Y);  //paragraph 2
+   image(advancedAssets[6], button2X, button2Y); //suggestion button
+   image(advancedAssets[7], button2X, nextY); //next page button
+}
+
+//draws images from thingstobuyArray
+drawThingstobuy = function() {
+   background(255, 232, 182); 
+
+   //images in array
+   image(thingstobuyAssets[0], maintitleX, maintitleY);  //subtitle
+   image(thingstobuyAssets[1], imageleftX, row1Y); //macrame
+   image(thingstobuyAssets[2], imagerightX, row1Y);  //humidifier
+   image(thingstobuyAssets[3], imageleftX, row2Y); //perlite
+   image(thingstobuyAssets[4], imagerightX, row2Y);  //spritzer  
+   image(thingstobuyAssets[5], imageleftX, textrow1Y);  //macrame text
+   image(thingstobuyAssets[6], imageleftX, textrow2Y); //humidifier text
+   image(thingstobuyAssets[7], imagerightX, textrow1Y);  //perlite text
+   image(thingstobuyAssets[8], imagerightX, textrow2Y); //spritzer text
+}
+
+// Change the drawFunction variable based on your interaction
+function keyPressed() {
+  // shows which was typed
+  //print(key);
+  print(keyCode);
+
+   // main page [m]
+  if( drawFunction === drawMainpage ) {
+    if( key === 'b' ) {
+      drawFunction = drawBeginner;
+    }
+  } 
+
+  // beginner page [b] 
+  else if( drawFunction === drawBeginner ) {
+      if( key === 't' ) {
+      drawFunction = drawThingstobuy;
+    }
+    if( key === 'i' ) {
+      drawFunction = drawIntermediate;
+    }
+    else if( key === 'm' ) {
+      drawFunction = drawMainpage;
+      }
+  }
+
+    // intermediate [i]
+  else if( drawFunction === drawIntermediate ) {
+
+      if( key === 't' ) {
+      drawFunction = drawThingstobuy;
+    }
+      if( key === 'a' ) {
+      drawFunction = drawAdvanced;
+    }
+      else if( key === 'b' ) {
+      drawFunction = drawBeginner;
+    }
+  }
+
+      // advanced [a]
+  else if( drawFunction === drawAdvanced ) {
+
+      if( key === 't' ) {
+      drawFunction = drawThingstobuy;
+      }
+      if( key === 'i' ) {
+      drawFunction = drawIntermediate;
+      }
+      if( key === 'm' ) {
+      drawFunction = drawMainpage;
+      }
+  }
+
+    // Things to buy [t]
+  else if( drawFunction === drawThingstobuy ) {
+      if( key === 'm') {
+      drawFunction = drawMainpage;
+    }
+      if( key === 'b' ) {
+      drawFunction = drawBeginner;
+    }
+      if( key === 'i' ) {
+      drawFunction = drawIntermediate;
+    }
+      else if( key === 'a' ) {
+      drawFunction = drawAdvanced;
+    }
+  }
 }
